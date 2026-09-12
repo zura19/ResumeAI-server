6de6277cf48f557bb124439c5cf893164c19c0eb
zura <zuramanagadze1@yahoo.com>
2026-09-12 22:16:36 +0400

    feat: update greeting message in getHello method

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): { message: string } {
-    return { message: 'Hello World!' };
+    return { message: 'Welcome to the ResumeAI API' };
   }
 }
